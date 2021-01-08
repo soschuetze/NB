19768b572272fe46952a3c1355ea7d3f0ff72d73
Sinclaire Schuetze <soschuetze@gmail.com>
2021-01-08 15:25:43 -0500

    Loaded in descriptions of cards into the tarot dictionary.

--- NB/TarotViewController.swift
+++ NB/TarotViewController.swift
@@ -12,6 +12,8 @@ struct ItemTarot {
 var tarotItemName: String
 }
 
+
+
 class TarotViewController: UIViewController {
 /*get the dimensions of the phone screen
      used later in resizing the cards*/
@@ -54,9 +56,30 @@ class TarotViewController: UIViewController {
     var number = 1
     var numberDouble = 1
     
-    var tarotDict:[Int: String] = [1:"magician", 2:"priestess", 3:"empress", 4:"emporer", 5:"fool", 6:"hierophant", 7:"lovers", 8:"chariot", 9:"strength", 10:"hermit", 11:"fortune", 12:"justice",13:"hangedman", 14:"death", 15:"temperance", 16:"devil", 17:"tower", 18:"star", 19:"moon", 20:"sun", 21:"judgement", 22:"world"]
+    var tarotDict = [1:["Magician","Themes: skill, manifestation, influence Description: You have skills others don’t and they set you apart. It’s a perfect time to overcome adversity, you already hold everything you need to move forward."],
+                     2:["High Priestess","Themes: intuition, compassion, knowledge Description: You are forced to listen to your intuition and follow your instincts. Look into yourself for guidance."],
+                     3:["Empress","Themes: nature, femininity, beauty Description: Draw on your feminine energy to connect with the world to create and find pleasure. Discover ways to creatively express yourself."],
+                     4:["Emporer","Themes: power, structure, leadership Description: You might adopt a fatherly role that demands leadership or authority. Remember to rule with a fair but firm hand."],
+                     5:["Fool","Themes: originality, new beginnings, innocence Description: You’re near the start of something new and exciting. This is something adventurous, maybe traveling or being in a place that you’ve never been to before."],
+                     6:["Hierophant","Themes: spiritual wisdom, conformity, and traditions Description: Representing spiritual beliefs, The Hierophant encourages you to learn fundamental principles of a trusted source. If you get this card, you might be taking a role of a teacher or mentor where others look to you for your wisdom."],
+                     7:["Lovers","Themes: harmony, relationships, choices Description: This card means you have a soul honoring connection with a loved one. You may have found your soulmate or life partner."],
+                     8:["Chariot","Themes: control, success, determination Description” You are channeling your inner ferocity and determination to manifest your goals. Draw on your willpower to overcome obstacles."],
+                     9:["Strength","Themes: courage, persuasion, power Description: You are fueled by inner strength, personal power, and inner calm. You are committed to what you do and do it while showing composure and maturity."],
+                     10:["Hermit","Themes: introspection, inner guidance, soul searching Description: Your sense of knowledge and truth is within yourself."],
+                     11:["Fortune","Themes: luck, opportunity, destiny Description: The wheel represents life. There is a big, positive change coming soon."],
+                     12:["Justice","Themes: balance, fairness, responsibility Description: This card brings justice as we know it. It does not care about outside situations and will fall wherever necessary."],
+                     13:["Hanged Man","Themes: letting go, suspension, breaking old patterns Description: This card signifies that you are suspended in time, perhaps surrendering to something that lies below."],
+                     14:["Death","Themes: endings, change, letting go Description: While this card most likely does not represent actual death, it can bring about a metaphorical death of something, bringing an increase of self-awareness and profound change."],
+                     15:["Temperance","Themes: balance, harmony, looking for divine intervention Description: You should consider avoiding extremes right now to best gain a sense of balance."],
+                     16:["Devil","Themes: fear, materialism, unhealthy relationships Description: You might be feeling extra drawn to materialistic things right now or feeling like you’re trapped in some way."],
+                     17:["Tower","Themes: destruction, crisis, liberation Description: The tower is a sign that quick, unforeseen destruction is near. This is likely something that will be very sudden."],
+                     18:["Star","Themes: hope, inspiration, opportunities Description: You are likely feeling inspired! This is a sign of renewed faith and might be feeling extra love from the universe."],
+                     19:["Moon","Themes: deception, fear, something is hidden Description: The moon signals deception. Something might not be the way that you see it."],
+                     20:["Sun","Themes: joy, success, vitality Description: The sun signals good fortune, happiness and harmony. The universe agrees with what you’re doing and is helping you along the way."],
+                     21:["Judgement","Themes: awakening, decision making, redemption Description: Signalling a sense of rebirth, you are ending a cycle of your life and starting to prepare for the next."],
+                     22:["World","Themes: achievement, fulfillment, possibility Description: The world signals the end of a cycle and you might feel a pause in your life. A big change is coming to take you to the next cycle."]]
    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
             shuffleCards()
@@ -84,10 +107,11 @@ class TarotViewController: UIViewController {
             /*determines whether cards will be "reversed" by getting a random number of either 1 or 2*/
             if flip == 1{
                 let cardNum = Int.random(in: 0..<tImages.count-1)//gets random number to determine which card in array is chosen
+                
                 tarotImage.image = UIImage(named: tImages[cardNum].tarotItemName)
                 self.tarotImage.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi)) //rotates card so that it is reversed
                 if number == (tImages.count)-1{
-                    cardLabel.text = tarotDict[cardNum] ?? "" + " reversed"
+                    cardLabel.text = tarotDict[cardNum]?[0] ?? "" + " reversed"
                 }
                 
             }else{
@@ -95,16 +119,15 @@ class TarotViewController: UIViewController {
                 tarotImage.image = UIImage(named: tImages[cardNum].tarotItemName)
                 self.tarotImage.transform = CGAffineTransform(rotationAngle: CGFloat(0))
                 if number == (tImages.count)-1{
-                cardLabel.text = tarotDict[cardNum] ?? ""
+                    cardLabel.text = tarotDict[cardNum]?[0] ?? ""
                 }
             }
-            
             number += 1
+            }
             
         }
-       
         
-    }
+    
  
     private func setupTarot(){
            
@@ -136,7 +159,7 @@ class TarotViewController: UIViewController {
                        doubleImage.image = UIImage(named: tImages[cardNum2].tarotItemName)
                        self.doubleImage.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi)) //rotates image so that it is reversed
                        if numberDouble == (tImages.count)-1{
-                       doubleLabel.text = tarotDict[cardNum2] ?? "" + " reversed"
+                        doubleLabel.text = tarotDict[cardNum2]?[0] ?? "" + " reversed"
                        }
                        
                    }else{
@@ -144,7 +167,7 @@ class TarotViewController: UIViewController {
                        doubleImage.image = UIImage(named: tImages[cardNum2].tarotItemName)
                        self.doubleImage.transform = CGAffineTransform(rotationAngle: CGFloat(0))
                        if numberDouble == (tImages.count)-1{
-                       doubleLabel.text = tarotDict[cardNum2] ?? ""
+                        doubleLabel.text = tarotDict[cardNum2]?[0] ?? ""
                        }
                    }
                    
@@ -176,3 +199,7 @@ class TarotViewController: UIViewController {
     */
 
 }
+
+    
+    
+
