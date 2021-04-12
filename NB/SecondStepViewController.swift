e209997c1e5e3d022773aff35b79309cf568b744
Sinclaire Schuetze <soschuetze@gmail.com>
2021-04-12 14:10:01 -0400

    Made edits to page view on onboarding screen. Removed navigation controller, will add back in and make edits so that the toolbar isn't affected on the second tarot screen.

--- NB/SecondStepViewController.swift
+++ NB/SecondStepViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class SecondStepViewController: UIViewController {
+class SecondStepViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var instructions: [String] = ["The future is in your hands","Your personal horoscope","Your voice matters"]
+    var frame = CGRect.zero
+    
     @IBAction func actionButtonTapped(_ sender: UIButton) {
         if let pageController = parent as? MainPageViewController{
             pageController.pushNext()
@@ -17,11 +23,34 @@ class SecondStepViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        pageControl.numberOfPages = instructions.count
+            setupScreens()
 
+            scrollView.delegate = self
         // Do any additional setup after loading the view.
     }
-    
+    func setupScreens() {
+        for index in 0..<instructions.count {
+            // 1.
+            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+            frame.size = scrollView.frame.size
+            
+            // 2.
+            let textLabel = UILabel(frame: frame)
+            textLabel.text = instructions[index]
+            textLabel.textAlignment = .center
 
+            self.scrollView.addSubview(textLabel)
+        }
+
+        // 3.
+        scrollView.contentSize = CGSize(width: (scrollView.frame.size.width * CGFloat(instructions.count)), height: scrollView.frame.size.height)
+        scrollView.delegate = self
+    }
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageControl.currentPage = Int(pageNumber)
+    }
     /*
     // MARK: - Navigation
 
